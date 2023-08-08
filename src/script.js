6d5592d2c6bf27af9a8c1532ca0edc0081a90984
OsamaAlwalily <oalwalily5@gmail.com>
2023-08-08 23:21:25 +0300

    Make favourite button and add alerts

--- src/script.js
+++ src/script.js
@@ -1,10 +1,11 @@
 //Define attributes
-// const favourite = document.getElementsByClassName("favouriteButton");
-const removeBtn = document.getElementsByClassName("removeButton"); //remove from cart
-const btns = document.getElementsByClassName("addBtn"); //Add buttons
-const removeValue = document.getElementsByClassName("removeBtn");
-const purchase = document.getElementById("purchase");
-// const heart = document.getElementById("heart0");
+const favourite = document.getElementsByClassName("favouriteButton"); //Add the attribute of favourite btn
+const removeBtn = document.getElementsByClassName("removeButton"); //Add the attribute of remove btn from cart
+const btns = document.getElementsByClassName("addBtn"); //Add the attribute of buttons
+const removeValue = document.getElementsByClassName("removeBtn"); //Add the attribute of remove btn of values
+const purchase = document.getElementById("purchase"); //Add the attribute of purchase btn
+
+//Define attributes
 let keyboard = 0,
   mouse = 0,
   keyboardPrice = 50,
@@ -12,20 +13,21 @@ let keyboard = 0,
   keyboardTotalPrice = 0,
   mouseTotalPrice = 0;
 
-//remove elements
+//remove element 1
 removeBtn[0].addEventListener("click", (e) => {
   e.preventDefault();
   const element = document.getElementsByClassName("keyboard")[0];
   element.remove();
 });
 
+//remove element 1
 removeBtn[1].addEventListener("click", (e) => {
   e.preventDefault();
   const element = document.getElementsByClassName("mouse")[0];
   element.remove();
 });
 
-//add elemnts
+//add elemnt 1
 btns[0].addEventListener("click", (e) => {
   e.preventDefault();
   keyboard = document.keyboardForm.keyboard.value;
@@ -37,6 +39,8 @@ btns[0].addEventListener("click", (e) => {
     } \n price: ${keyboardTotalPrice} $`;
   }
 });
+
+//add elemnt 2
 btns[1].addEventListener("click", (e) => {
   e.preventDefault();
   mouse = document.mouseForm.mouse.value;
@@ -49,7 +53,7 @@ btns[1].addEventListener("click", (e) => {
   }
 });
 
-//remove values
+//remove values 1
 removeValue[0].addEventListener("click", () => {
   const kTotaL = document.getElementById("kTotal");
   if (keyboard > 0) {
@@ -65,6 +69,8 @@ removeValue[0].addEventListener("click", () => {
     } \n price: ${keyboardTotalPrice} $`;
   }
 });
+
+//remove values 2
 removeValue[1].addEventListener("click", () => {
   const mTotaL = document.getElementById("mTotal");
   if (mouse > 0) {
@@ -79,7 +85,22 @@ removeValue[1].addEventListener("click", () => {
     } \n price: ${mouseTotalPrice} $`;
   }
 });
+
+//purchase btns
 purchase.addEventListener("click", () => {
   const total = document.getElementById("total");
-  total.innerText = `Total price is: ${keyboardTotalPrice + mouseTotalPrice}`;
+  total.innerText = `Total price is: ${keyboardTotalPrice + mouseTotalPrice} $`;
+  keyboardTotalPrice + mouseTotalPrice <= 0 ? alert("No products") : null;
+});
+
+//favourite btn 1
+favourite[0].addEventListener("click", () => {
+  const heart = document.getElementsByClassName("heart")[0];
+  heart.classList.toggle("active");
+});
+
+//favourite btn 2
+favourite[1].addEventListener("click", () => {
+  const heart = document.getElementsByClassName("heart")[1];
+  heart.classList.toggle("active");
 });
